8d244ebf101e0dd1dc5e52d7af290c6981486e99
Andrea Bizzotto <bizz84@gmail.com>
2016-03-17 19:00:42 +0000

    Initial Commit

---
+++ MVHorizontalPicker/MVHorizontalPicker.h
@@ -0,0 +1,19 @@
+//
+//  MVHorizontalPicker.h
+//  MVHorizontalPicker
+//
+//  Created by Andrea Bizzotto on 17/03/2016.
+//  Copyright © 2016 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVHorizontalPicker.
+FOUNDATION_EXPORT double MVHorizontalPickerVersionNumber;
+
+//! Project version string for MVHorizontalPicker.
+FOUNDATION_EXPORT const unsigned char MVHorizontalPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVHorizontalPicker/PublicHeader.h>
+
+
