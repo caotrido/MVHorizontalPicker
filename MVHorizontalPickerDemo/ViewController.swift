071b0daccd40558b5a73931a91ea112bb7cf0748
Andrea Bizzotto <bizz84@gmail.com>
2016-03-19 16:33:32 +0000

    Added border width configurable property selector

--- MVHorizontalPickerDemo/ViewController.swift
+++ MVHorizontalPickerDemo/ViewController.swift
@@ -13,16 +13,22 @@ class ViewController: UIViewController {
 
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var picker: MVHorizontalPicker!
+    
+    
     @IBOutlet var itemWidthSelector: UISegmentedControl!
+    @IBOutlet var fontSizeSelector: UISegmentedControl!
+    @IBOutlet var borderWidthSelector: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        picker.font = UIFont.boldSystemFontOfSize(18)
-    
         picker.titles = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
 
         picker.itemWidth = itemWidth(itemWidthSelector)
+
+        picker.font = UIFont.boldSystemFontOfSize(fontSize(fontSizeSelector))
+        
+        picker.borderWidth = borderWidth(borderWidthSelector)
 
         updateLabel()
     }
@@ -42,7 +48,17 @@ class ViewController: UIViewController {
 
         picker.itemWidth = itemWidth(itemWidthSelector)
     }
+
+    @IBAction func fontSizeChanged(sender: AnyObject) {
+        
+        picker.font = UIFont.boldSystemFontOfSize(fontSize(fontSizeSelector))
+    }
     
+    @IBAction func borderWidthChanged(sender: AnyObject) {
+        
+        picker.borderWidth = borderWidth(borderWidthSelector)
+    }
+
     func itemWidth(itemWidthSelector: UISegmentedControl) -> CGFloat {
         switch itemWidthSelector.selectedSegmentIndex {
         case 0: return 100
@@ -50,6 +66,19 @@ class ViewController: UIViewController {
         case 2: return 150
         default: return 100
         }
+    }
+    
+    func fontSize(fontSizeSelector: UISegmentedControl) -> CGFloat {
+        switch fontSizeSelector.selectedSegmentIndex {
+        case 0: return 14.0
+        case 1: return 16.0
+        case 2: return 18.0
+        default: return 18.0
+        }
+    }
+    
+    func borderWidth(borderWidthSelector: UISegmentedControl) -> CGFloat {
+        return CGFloat(borderWidthSelector.selectedSegmentIndex)
     }
 }
 
